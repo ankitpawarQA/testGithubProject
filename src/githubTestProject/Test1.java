10ec3444323f7aafdd68ef7e0ec925823ba7e089
Akshu-QA <akshukalia26@gmail.com>
2025-09-07 10:45:17 +0530

    1stclone

--- src/githubTestProject/Test1.java
+++ src/githubTestProject/Test1.java
@@ -4,8 +4,8 @@ public class Test1 {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i <= 10; i++) {
-			if (i == 5) {
+		for (int i = 0; i <= 5; i++) {
+			if (i == 3) {
 				continue;
 			}
 			System.out.println(i);
