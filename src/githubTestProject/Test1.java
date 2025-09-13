1298edbe619777ad96ca4a51100758a9043fceb9
Akshu-QA <akshukalia26@gmail.com>
2025-09-13 10:17:19 +0530

    13-9 commit 3

--- src/githubTestProject/Test1.java
+++ src/githubTestProject/Test1.java
@@ -7,6 +7,9 @@ public class Test1 {
 	public void m2() {
 		System.out.println("hello javaa");
 	}
+	public void m3() {
+		System.out.println("hellom3");
+	}
 
 
 	public static void main(String[] args) {
